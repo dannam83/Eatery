898b0d0587ad9621fb4125b2906257b27d0bcc59
Daniel Nam <dannam83@gmail.com>
2017-10-29 01:34:55 -0400

    add pieces to default header

--- frontend/components/main_header.jsx
+++ frontend/components/main_header.jsx
@@ -2,48 +2,49 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const logo = () => (
-  <header className="session">
-    <Link to="/" className="logo">Eatery</Link>
+  <div className="logo-default-div">
+    <Link to="/" className="logo-default">Eatery</Link>
     <a href="/">
-      <img className="chefPic" src={window.chefLogo}></img>
+      <img className="chefPic" id="default-chef" src={window.chefLogo}></img>
     </a>
-  </header>
+  </div>
 );
 
 const sessionLinks = () => (
-  <nav className="login-signup">
-    <h1>This header for everything else</h1>
-    <Link className="session-login" to="/login">Log In</Link>
-      &nbsp;&nbsp;
-    <Link className="session-signup" to="/signup">Sign Up</Link>
-  </nav>
+  <div className="signup-default-div">
+    <Link className="signup-default" to="/signup">Sign Up</Link>
+  </div>
 );
 
 const greeter = (currentUser, logout) => (
-  <nav className="header-greeter">
-    <h1>This header for everything else</h1>
+  <div className="greeter-default-div">
     <h3>{currentUser.fname} {currentUser.lname[0]}</h3>
-    <Link onClick={logout} to="/login">Log Out</Link>
-  </nav>
+    <Link onClick={logout} to="/login" className="logout-default">Log Out</Link>
+  </div>
 );
 
-const banner = (currentUser, logout, location) => {
-  const sessions = currentUser ? greeter(currentUser, logout) : sessionLinks();
+const searchBar = () => {
   return (
-    <div className="header-container">
-      <img className="star-logo" src={window.starLogo}></img>
-      {logo()}
-
-      {sessions}
+    <div className="search-default-div">
+      <span className="search-default-label" id="search-default-first">Find</span>
+      <input className="search-default-input" type="text" placeholder="burgers, deli, italian, etc."/>
+      <span className="search-default-label" id="search-default-second"><p className="short-default-border">Near</p></span>
+      <input className="search-default-input" type="text" placeholder="City, State"/>
+      <input className="search-default-submit" id="search-default-last" value="Go"/>
     </div>
   );
 };
 
-// interpolate later, took away interpolation to allow linter
 const MainHeader = ({currentUser, logout, location}) => {
+  const userPresence = currentUser ? greeter(currentUser, logout) : sessionLinks();
   return (
-    <div>
-      {banner(currentUser, logout, location)}
+    <div className="header-container">
+      <img className="star-logo" src={window.starLogo}></img>
+      <header className="header-default">
+        {logo()}
+        {searchBar()}
+        {userPresence}
+      </header>
     </div>
   );
 };
