bf9bb653776cdc81fe594d98b35e569eac034e32
Daniel Nam <dannam83@gmail.com>
2017-11-20 13:26:47 -0500

    add review seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,94 +1,333 @@
 require 'faker'
 
-Biz.create(
-  name: 'Fancy Smancy',
-  lat: 40.778534,
-  lng:  -73.983597,
-  hours: {
-    Mon:["5:00 pm","11:30 pm"],
-    Tue:["5:00 pm","11:30 pm"],
-    Wed:["5:00 pm","11:30 pm"],
-    Thu:["5:00 pm","11:30 pm"],
-    Fri:["5:00 pm","11:30 pm"],
-    Sat:["5:00 pm","11:30 pm"],
-    Sun:["5:00 pm","11:30 pm"],
-  },
-  price: 4,
-  phone: "(646) 771-9372",
-  biz_image: File.open('app/assets/images/restaurant/fancysmancy.jpg')
-)
 
-Description.create(
-  biz_id: Biz.find_by(name: 'Fancy Smancy').id,
-  category: 'Seafood'
-)
-Description.create(
-  biz_id: Biz.find_by(name: 'Fancy Smancy').id,
-  category: 'Lobster'
-)
-Description.create(
-  biz_id: Biz.find_by(name: 'Fancy Smancy').id,
-  category: 'Salads'
-)
 
-Biz.create(
-  name: 'Pastry Town',
-  lat: 40.779079,
-  lng: -73.978662,
-  hours: {
-    Mon:["10:00 am","11:30 pm"],
-    Tue:["10:00 am","11:30 pm"],
-    Wed:["10:00 am","11:30 pm"],
-    Thu:["10:00 am","11:30 pm"],
-    Fri:["10:00 am","1:30 am"],
-    Sat:["11:00 am","1:30 am"],
-    Sun:["11:00 am","11:30 pm"],
-  },
-  price: 2,
-  phone: "(646) 865-9372",
-  biz_image: File.open('app/assets/images/restaurant/pastrytown.jpg')
-)
 
-Description.create(
-  biz_id: Biz.find_by(name: 'Pastry Town').id,
-  category: 'Cupcakes'
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
-Description.create(
-  biz_id: Biz.find_by(name: 'Pastry Town').id,
-  category: 'Cakes'
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
-Description.create(
-  biz_id: Biz.find_by(name: 'Pastry Town').id,
-  category: 'Desserts'
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
-
-Biz.create(
-  name: 'I <3 Burgers',
-  lat: 40.768184,
-  lng: -73.986233,
-  hours: {
-    Mon:["10:00 am","11:30 pm"],
-    Tue:["10:00 am","11:30 pm"],
-    Wed:["10:00 am","11:30 pm"],
-    Thu:["10:00 am","11:30 pm"],
-    Fri:["10:00 am","1:30 am"],
-    Sat:["11:00 am","1:30 am"],
-    Sun:["11:00 am","11:30 pm"],
-  },
-  price: 3,
-  phone: "(646) 199-9372",
-  biz_image: File.open('app/assets/images/restaurant/iLoveBurgers.jpg')
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
-
-Description.create(
-  biz_id: Biz.find_by(name: 'I <3 Burgers').id,
-  category: 'Burgers'
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
-Description.create(
-  biz_id: Biz.find_by(name: 'I <3 Burgers').id,
-  category: 'American (Traditional)'
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
-Description.create(
-  biz_id: Biz.find_by(name: 'I <3 Burgers').id,
-  category: 'Sandwiches'
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::FamilyGuy.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HarryPotter.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::Friends.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: "This place is so good. I've tried other place like this and
+  this one is the best. From the moment that I came in they treated
+  me like family and it was like that our whole visit. Definitely come
+  check this place out if you want good food and good service!",
+  rating: 5,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: "Holy smokes! My mind is completely blown. How can food be so
+  good?!?! If I had to choose one thing to eat for the rest of my life,
+  be this, hands down! I could eat it today and tomorrow and tomorrow
+  and the all the tomorrows for the rest of my life. Best food ever!!!",
+  rating: 5,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: "Gimme a G! Gimme an R! Gimme an E! Gimme an A! Gimme a T!
+  Because this place is great!!! It reminds me of, wait, it doesn't
+  remind me of anything because I've never had anything this good before!
+  Make sure to visit this place. You won't regret it! I promise my left
+  hand on it!",
+  rating: 5,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: "Terrible. Absolutely terrible. The appetizer was dry and tasted
+  like it was leftovers from last week. The entre was equally dissatisfying.
+  Bland and boring and left me wanting a LOT more. I'm thankful that I
+  didn't get sick because I really wondered to myself if this was last
+  week's food. Don't come here. Save yourself the time and the stress.",
+  rating: 1,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: "Food was okay honestly. It wasn't great, but it was decent.
+  I'm still giving this place one star though because the service was
+  TERRIBLE! The greeter at the door was rude, and our waiter seriously
+  never checked on us. We had to wave him down every single time. You
+  would think that the server would at least take care of the things that
+  we had to flag him down for, but nope. He completely forgot and we
+  basically had to ask him twice for everything. Never coming here again.",
+  rating: 1,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: "So, I'm not really sure how to rate this place. There were aspects
+  that were really good, and couple that were really bad. First of all,
+  the food is good! It was one of those moments where the first bite
+  gives you that wow reaction. So no complaints about the food. Also, the
+  service was good too. Our server made sure we always had water and
+  always did with a smile. But....they really could clean up the place. I
+  couldn't help but wonder how they're inspections went, because, it feels
+  really dirty in there. And the bathrooms.....woah....although the food
+  was good. I can't see myself going back because of the bathrooms.",
+  rating: 2,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
+)
+Review.create(
+  body: Faker::HowIMetYourMother.quote,
+  rating: [1,2,3,4,5].sample,
+  user_id: User.all[1..-1].sample.id,
+  biz_id: Biz.all.sample.id
 )
