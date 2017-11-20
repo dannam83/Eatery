1e2381beffe68d8edc4a3264618253dabaa508b8
Daniel Nam <dannam83@gmail.com>
2017-11-20 11:51:38 -0500

    try adjusting reviews reducer

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -3,12 +3,12 @@ import merge from 'lodash/merge';
 
 const ReviewsReducer = (state = {}, action) => {
   Object.freeze(state);
-  const reviews = action.biz.reviews;
+
   let newState = merge({}, state);
   switch (action.type) {
     case RECEIVE_BIZ:
-      if (reviews) {
-        return reviews;
+      if (action.biz.reviews) {
+        return action.biz.reviews;
       } else {
         return [];
       } break;
